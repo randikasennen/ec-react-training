dc56116adaa3641567e364188e71fd73103c1f1e
Sennen Randika <randikasennen@gmail.com>
2021-03-05 11:11:18 +0530

    Session 03 - Functional Components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+    const [count, setCount] = useState(0);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    return(
+        <div>
+            <div>Count: {count}</div>
+            <button onClick={() => setCount(count + 1)}>Count</button>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
