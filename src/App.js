2036be6e24a52ef5ddc355e9f8c9b81aa3cecbc1
Sennen Randika <randikasennen@gmail.com>
2021-03-10 10:48:27 +0530

    Form validation [with error]

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import './App.css';
 import TextInput from './components/TextInput';
 
 export default function App() {
@@ -6,13 +7,54 @@ export default function App() {
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
 
+  const [firstNameError, setFirstNameError] = useState(null);
+  const [lastNameError, setLastNameError] = useState(null);
+  const [emailError, setEmailError] = useState(null);
+
   const handleOnSubmit = (e) => {
     e.preventDefault();
 
-    console.log(firstName);
-    console.log(lastName);
-    console.log(email);
+    const isValid = validateForm();
+
+    if(isValid) {
+      console.log("Form is valid");
+    }
+    else {
+      console.log("Form is invalid");
+    }
   }
+
+  const validateForm = () => {
+    if(!firstName.trim().length) {
+      setFirstNameError("First name is required");
+    }
+
+    if(!lastName.trim().length) {
+      setLastNameError("Last name is required");
+    }
+
+    if(!_validateEmail()) {
+      setEmailError("Invalid email address");
+    }
+
+    if(!firstNameError && !lastNameError && !emailError) {
+      return true;
+    }
+
+    return false;
+  }
+
+  const _validateEmail = () => {
+    const pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+  
+    if(!pattern.test(email)) {
+      return false;
+    }
+
+    return true;
+  }
+
+
 
   return (
     <div>
@@ -23,6 +65,7 @@ export default function App() {
           name="first_name"
           value={firstName}
           onChange={(value) => setFirstName(value)}
+          error={firstNameError}
         />
         
         <TextInput
@@ -30,6 +73,7 @@ export default function App() {
           name="last_name"
           value={lastName}
           onChange={(value) => setLastName(value)}
+          error={lastNameError}
         />
         
         <TextInput
@@ -37,9 +81,10 @@ export default function App() {
           name="email"
           value={email}
           onChange={(value) => setEmail(value)}
+          error={emailError}
         />
 
-        <input type="submit" />
+        <input type="submit" className="submit"/>
       </form>
     </div>
   );
--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -1,12 +1,15 @@
 import './index.css';
 
 export default function TextInput(props) {
-    const { label, name, value, onChange } = props;
+    const { label, name, value, onChange, error } = props;
 
     return(
-        <div className="text-input">
-          <div className="label">{label}</div>
-          <input type="text" name={name} value={value} onChange={(e) => onChange(e.target.value)} />
+        <div className="text-input-container">
+            <div className="text-input">
+                <div className="label">{label}</div>
+                <input type="text" name={name} value={value} onChange={(e) => onChange(e.target.value)} /> 
+            </div>
+            {error && <div className="error">{error}</div>}
         </div>
     )
 }
