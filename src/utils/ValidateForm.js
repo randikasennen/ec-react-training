781c77395636f708db8aae2e663268b8f678d48a
Sennen Randika <randikasennen@gmail.com>
2021-03-10 11:55:41 +0530

    Fix error with email validation

--- src/utils/ValidateForm.js
+++ src/utils/ValidateForm.js
@@ -34,7 +34,7 @@ export default function ValidateForm(data) {
 function _validateEmail(email) {
     const pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
   
-    if(!pattern.test(email)) {
+    if(!pattern.test(email.trim())) {
       return false;
     }
 
