e8197ba36f9c1bc25140a90d4f558612ad69cb51
Sennen Randika <randikasennen@gmail.com>
2021-03-19 10:49:48 +0530

    Component Arc

--- src/pages/UserPage/index.js
+++ src/pages/UserPage/index.js
@@ -1,15 +1,23 @@
+import './index.css';
+
 export default function(props) {
     const { user } = props.location.state
     const { id, email, first_name, last_name, avatar } = user;
 
-    console.log(props)
-
     return (
-        <div>
-            <div>{id}</div>
-            <div>{email}</div>
-            <div>{first_name}</div>
-            <div>{last_name}</div>
+        <div className="card">
+            <img className="cover-image" src="https://www.incimages.com/uploaded_files/image/1920x1080/getty_509107562_2000133320009280346_351827.jpg" />
+            
+            <div className="user-info-container">
+                <img className="avatar" src={avatar} />
+
+                <div className="user-info">
+                    <div className="name">{first_name + ' ' + last_name}</div>
+                    <div className="email">{email}</div>
+                </div>
+            </div>
+
+            <div className="description">Use the sample job postings below to help write your job description and improve your job posting results.</div>
         </div>
     )
 }
